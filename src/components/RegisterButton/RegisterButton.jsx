315f747d4bc7e3842c216d5430ccef7ecb2b99ed
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-08-04 16:42:20 -0500

    interest form

--- src/components/RegisterButton/RegisterButton.jsx
+++ src/components/RegisterButton/RegisterButton.jsx
@@ -1,7 +1,15 @@
 import "./RegisterButton.css";
 
 const RegisterButton = () => {
-  return <button className="register-button">Register</button>;
+  return (
+    <a
+      href="https://docs.google.com/forms/d/e/1FAIpQLSeawF6v2f9KHdGdV4mZW_coewp26g9eQWOtQYOtBr8h8ltmBg/viewform"
+      className="register-button"
+      rel="noreferrer"
+      target="_blank">
+      Interest Form
+    </a>
+  );
 };
 
 export default RegisterButton;
--- src/components/RegisterNowBtn/RegisterNowBtn.jsx
+++ src/components/RegisterNowBtn/RegisterNowBtn.jsx
@@ -3,7 +3,13 @@ import "./RegisterNowBtn.css";
 const RegisterNowBtn = () => {
   return (
     <div className="register-now-btn-container">
-      <button className="register-now">Register before November 1st</button>
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSeawF6v2f9KHdGdV4mZW_coewp26g9eQWOtQYOtBr8h8ltmBg/viewform"
+        className="register-now"
+        rel="noreferrer"
+        target="_blank">
+        Fill out our interest form now!
+      </a>
     </div>
   );
 };
