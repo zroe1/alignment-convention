6ef271507be5155bc4193bd2b5aee26820ea1a04
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-08-04 19:31:43 -0500

    filler for about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,21 @@
 import "../Subpage.css";
+import FAQ from "../FQA/FQA";
 
 const About = () => {
   return (
     <div className="sub-page-container">
       <h2>About AlignCon</h2>
+      <div className="sub-page-text-container">
+        <p className="sub-page-text">
+          AlignCon is a conference that brings together experts in artificial intelligence, machine
+          learning, and data science to discuss the future of technology. Our goal is to create a
+          space where professionals can share their knowledge and collaborate on innovative
+          projects. We believe that by working together, we can build a better future for everyone.
+          Join us at AlignCon to learn from the best and help shape the future of technology. More
+          text here...
+        </p>
+      </div>
+      <FAQ />
     </div>
   );
 };
